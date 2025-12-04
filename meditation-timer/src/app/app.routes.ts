3b1c756fbf44dd6381e7181336ca46dc7539b856
kiev78 <eugenepevzner@gmail.com>
2025-12-03 22:39:19 -0500

    add donation page

--- meditation-timer/src/app/app.routes.ts
+++ meditation-timer/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { TimerContainerComponent } from './timer-container/timer-container.component';
 import { ReadingsComponent } from './readings/readings.component';
 import { SettingsComponent } from './settings/settings.component';
+import { DonateComponent } from './donate/donate.component';
 
 export const routes: Routes = [
   { path: '', component: TimerContainerComponent },
   { path: 'timer', component: TimerContainerComponent },
   { path: 'readings', component: ReadingsComponent },
-  { path: 'settings', component: SettingsComponent }
+  { path: 'settings', component: SettingsComponent },
+  { path: 'donate', component: DonateComponent }
 ];
---
+++ meditation-timer/src/app/donate/donate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-donate',
+  templateUrl: './donate.component.html',
+  styleUrls: ['./donate.component.css'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class DonateComponent {}
