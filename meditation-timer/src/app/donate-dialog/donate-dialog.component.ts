34e4a9a2aa506446eb06135594d5aae8b741f0b2
kiev78 <eugenepevzner@gmail.com>
2025-12-04 20:28:01 -0500

    celanup

--- meditation-timer/src/app/donate-dialog/donate-dialog.component.ts
+++ meditation-timer/src/app/donate-dialog/donate-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MatDialogRef, MatDialogContent, MatDialogActions, MatDialogClose } from '@angular/material/dialog';
+import { MatDialogRef, MatDialogContent, MatDialogActions } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
@@ -7,7 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './donate-dialog.component.html',
   styleUrls: ['./donate-dialog.component.css'],
   standalone: true,
-  imports: [MatDialogContent, MatDialogActions, MatButtonModule, MatDialogClose]
+  imports: [MatDialogContent, MatDialogActions, MatButtonModule]
 })
 export class DonateDialogComponent {
   constructor(public dialogRef: MatDialogRef<DonateDialogComponent>) {}
